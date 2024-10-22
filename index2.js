30ae5cc14933b67161329ddb9821a79d13ac8f7e
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-10-22 17:09:28 +0530

    task 1,2,3 else if esle

--- index2.js
+++ index2.js
@@ -1,18 +1,78 @@
-let a1 = "asxsf"
-let b1 = 50
-console.log(a1 + b1)
-let a2 = "465"
-let b2 = 50
-console.log(a2 - b2)
-let a3 = "85"
-let b3 = 50
-console.log(a3 * b3)
-let a4 = "asxsf"
-let b4 = 50
-console.log(a4 / b4)
-let a5 = "asxsf"
-let b5 = 50
-console.log(a5 * b5)
-let a6 = "800"
-let b6 = 50
-console.log(a6 / b6)
+
+// string concateination
+
+// let a1 = "asxsf"
+// let b1 = 50
+// console.log(a1 + b1)
+// let a2 = "465"
+// let b2 = 50
+// console.log(a2 - b2)
+// let a3 = "85"
+// let b3 = 50
+// console.log(a3 * b3)
+// let a4 = "asxsf"
+// let b4 = 50
+// console.log(a4 / b4)
+// let a5 = "asxsf"
+// let b5 = 50
+// console.log(a5 * b5)
+// let a6 = "800"
+// let b6 = 50
+// console.log(a6 / b6)
+
+
+// task 1 odd and even Number
+// const prompt = require ("prompt-sync")()
+// let number = prompt("enter your numnber__");
+// // use npm package prompt-sync  
+
+// if(number % 2 === 0){
+//     console.log(number + " is even number")
+// }
+// else{
+//     console.log(number + "is odd number")
+// }
+
+// task 2 user input wla 
+
+// const prompt = require ("prompt-sync")()
+// let named = prompt("enter your named is__");
+
+// if(named === "Devanshu"){
+//     console.log("dev")
+// }
+// else if (named === "Dev"){
+//     console.log("devanshu")
+// }
+// else if (named === "kizie"){
+//     console.log("manny")
+// }
+// else if (named === "manny"){
+//     console.log("kizie")
+// }
+// else{
+//     console.log("invalid")
+// }
+
+// task 3 
+
+const prompt = require ("prompt-sync")()
+let a = parseInt(prompt("enter your a is__"));
+let b = parseInt(prompt("enter your b is__"));
+let operator = prompt("enter your operator is__");
+
+if(operator === "+"){
+    console.log(a+b)
+}
+else if (operator === "-"){
+    console.log(a-b)
+}
+else if (operator === "*"){
+    console.log(a*b)
+}
+else if (operator === "/"){
+    console.log(a/b)
+}
+else{
+    console.log("invalid")
+}
