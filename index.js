63339e01f6d13752e5f8d180e433a68b902be57a
Divyyanshu <divvyyanshuutailor@gmail.com>
2024-10-22 13:13:05 +0530

    add js in grras internship

---
+++ index.js
@@ -0,0 +1,12 @@
+let age = 5
+
+if (age > 18) {
+    console.log("eleigible for vote")
+}
+else if (age < 17) {
+    console.log("you are not eligilbe for vote")
+}
+else {
+    console.log("blank null")
+}
+
---
+++ index2.js
@@ -0,0 +1,18 @@
+let a1 = "asxsf"
+let b1 = 50
+console.log(a1 + b1)
+let a2 = "465"
+let b2 = 50
+console.log(a2 - b2)
+let a3 = "85"
+let b3 = 50
+console.log(a3 * b3)
+let a4 = "asxsf"
+let b4 = 50
+console.log(a4 / b4)
+let a5 = "asxsf"
+let b5 = 50
+console.log(a5 * b5)
+let a6 = "800"
+let b6 = 50
+console.log(a6 / b6)
